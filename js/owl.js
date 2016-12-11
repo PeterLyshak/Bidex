a8b12e96ced0956a97bbe3fd241384b9d671e4cf
Peter Lyshak <lishak09@gmail.com>
2016-12-11 14:11:11 +0200

    s

---
+++ js/owl.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+  $("#carousel").owlCarousel({
+	  pagination: true,
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem : true,
+			autoPlay: 4000,
+          navigationText: []
+  });
+	
+});
+
+
