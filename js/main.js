082412d7d62aefba74ddebe5859f3056463a2eaf
PeterLyshak <petermac@MacBook-Pro-Peter.local>
2017-03-02 09:39:11 +0200

    Bug fixes
    - Dashboard table fix

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
- 
+
   $("#carousel").owlCarousel({
 	  pagination: true,
           slideSpeed : 300,
           paginationSpeed : 400,
           singleItem : true,
-			autoPlay: 4000,
+	        autoPlay: 4000,
           navigationText: []
   });
-	
+
 	$(".select").styler({});
-	
+
    	$('.page-header__toggle').click(function(){
 	$(".page-header__nav").toggleClass("page-header__nav--opened");
    });
-	
+
 	$('.spoiler__body').hide();
 	$('.spoiler__title').click(function(){
     $(this).next().slideToggle();
 });
 
-	
+
 });
